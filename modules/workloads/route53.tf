e66f6e30c6818d441a1e03cd8ebd650c792f94c3
Jack Rudenko <mail@erudenko.com>
2023-06-26 21:33:16 +1000

    fox api DNS name for prod

--- modules/workloads/route53.tf
+++ modules/workloads/route53.tf
@@ -39,7 +39,7 @@ resource "aws_service_discovery_service" "mockoon" {
 
 resource "aws_route53_record" "api" {
   zone_id = var.zone_id
-  name = "api.${var.env}.${var.domain}"
+  name = "api.${var.env == "prod" ? "app" : var.env}.${var.domain}"
   type = "CNAME"
   ttl = 60
   records = [aws_lb.alb.dns_name]
