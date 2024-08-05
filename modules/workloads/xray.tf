373a129c465ef82a2ff4eabf2d2abfd67b67a3bd
Jack Rudenko <i@madappgang.com>
2024-08-05 18:57:56 +1000

    replace xray daemon with otel agent

--- modules/workloads/xray.tf
+++ modules/workloads/xray.tf
@@ -1,22 +1,49 @@
-
 locals {
-  xray_container = var.xray_enabled ? [{
-    name  = "xray-daemon"
-    image = "amazon/aws-xray-daemon"
-    portMappings = [{
-      containerPort = 2000
-      hostPort      = 2000
-      protocol      = "udp"
-    }]
-    cpu               = 32
-    memoryReservation = 256
-    essential         = false
-  }] : []
+  xray_container = var.xray_enabled ?  [{
+      name  = "adot-collector"
+      image = "public.ecr.aws/aws-observability/aws-otel-collector:latest"
+      portMappings = [
+        {
+          containerPort = 2000
+          hostPort      = 2000
+          protocol      = "udp"
+        },
+        {
+          containerPort = 4317
+          hostPort      = 4317
+        },
+        {
+          containerPort = 4318
+          hostPort      = 4318
+        },
+        {
+          containerPort = 55681
+          hostPort      = 55681
+        }
+      ]
+      command = [
+        "--config=/etc/ecs/container-insights/otel-task-metrics-config.yaml"
+      ]
+      environment = [
+        {
+          name  = "AWS_REGION"
+          value = "us-west-2"  # Change this to your desired region
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/adot-collector"
+          awslogs-region        = "us-west-2"  # Change this to your desired region
+          awslogs-stream-prefix = "xray"
+        }
+      }
+    }] : []
 
   app_container_environment = var.xray_enabled ? [
     {
-      name  = "AWS_XRAY_DAEMON_ADDRESS"
-      value = "xray-daemon:2000"
+      name  = "ADOT_COLLECTOR_URL"
+      value = "localhost:2000"
     }
   ] : []
 }
