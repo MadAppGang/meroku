ad212ecbd24a6e54a1a80bc83c7233dd2974dab4
Jack Rudenko <i@madappgang.com>
2026-01-12 21:04:06 +1100

    fix(workloads): update services.tf to use unconditional SES policy reference

--- modules/workloads/services.tf
+++ modules/workloads/services.tf
@@ -291,12 +291,12 @@ resource "aws_iam_role_policy_attachment" "service_task_bucket" {
   policy_arn = aws_iam_policy.full_access_to_backend_bucket.arn
 }
 
-# SES access (conditional on ses_enabled)
+# SES access for all services
 resource "aws_iam_role_policy_attachment" "service_task_ses" {
-  for_each = { for k, v in local.service_names : k => v if var.ses_enabled }
+  for_each = local.service_names
 
   role       = aws_iam_role.services_task_execution[each.key].name
-  policy_arn = aws_iam_policy.send_emails[0].arn
+  policy_arn = aws_iam_policy.send_emails.arn
 }
 
 
